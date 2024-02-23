025390e48b59da8a548cbf9ffa1995cbdc174e54
Himanshu kumar <himanshu064@gmail.com>
2024-02-23 13:58:22 +0530

    Add All components and set layout and install packages and create services files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { SimpleHttpComponent } from './screens/simple-http/simple-http.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { SharedService } from './services/shared.service';
+import { ScheduleComponent } from './screens/schedule/schedule.component';
+import { SidebarComponent } from './common/sidebar/sidebar.component';
+import { HeaderComponent } from './common/header/header.component';
+import { CollectionsComponent } from './pages/collections/collections.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleHttpComponent,
+    LayoutComponent,
+    ScheduleComponent,
+    SidebarComponent,
+    HeaderComponent,
+    CollectionsComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+   
   ],
-  providers: [],
+  providers: [ SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
